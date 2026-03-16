46e3ad0f0807d8613f48f18d108b203eb56649c2
Rowell Indino (r.indino) <rowell.indino@neu.edu.ph>
2026-03-16 20:51:13 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve '@/

--- src/components/check-in-form.tsx
+++ src/components/check-in-form.tsx
@@ -5,7 +5,7 @@ import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as z from 'zod';
 import { Button } from "@/components/ui/button";
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/form-provider";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { Input } from "@/components/ui/input";
 import { Card, CardHeader, CardTitle, CardDescription, CardContent } from "@/components/ui/card";
