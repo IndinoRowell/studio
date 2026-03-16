2ca1f96b3add26878d55dff0009d310d287372cf
Rowell Indino (r.indino) <rowell.indino@neu.edu.ph>
2026-03-16 23:00:23 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyAPfsD3CYbU6e66UTJcZVG5fWSra-fYfuI",
-  authDomain: "studio-2543548682-44d30.firebaseapp.com",
-  projectId: "studio-2543548682-44d30",
-  storageBucket: "studio-2543548682-44d30.firebasestorage.app",
-  messagingSenderId: "359400501421",
-  appId: "1:359400501421:web:b238772ba5225e622c1624"
+  "projectId": "studio-2543548682-44d30",
+  "appId": "1:359400501421:web:b238772ba5225e622c1624",
+  "apiKey": "AIzaSyAPfsD3CYbU6e66UTJcZVG5fWSra-fYfuI",
+  "authDomain": "studio-2543548682-44d30.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "359400501421"
 };
