77a735eaf72997276839705adfa7c7109c336d3e
Rowell Indino (r.indino) <rowell.indino@neu.edu.ph>
2026-03-16 21:56:06 +0000

    const firebaseConfig = {   apiKey: "AIzaSyAPfsD3CYbU6e66UTJcZVG5fWSra-fY

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,8 @@
-
 export const firebaseConfig = {
-  apiKey: "AIzaSyAs-V0m-V1-X2-Y3-Z4-W5-V6-U7-T8-S9-R0",
-  authDomain: "neulib-connect.firebaseapp.com",
-  projectId: "neulib-connect",
-  storageBucket: "neulib-connect.firebasestorage.app",
-  messagingSenderId: "782194301234",
-  appId: "1:782194301234:web:9c2b4a1f0d3e5867"
+  apiKey: "AIzaSyAPfsD3CYbU6e66UTJcZVG5fWSra-fYfuI",
+  authDomain: "studio-2543548682-44d30.firebaseapp.com",
+  projectId: "studio-2543548682-44d30",
+  storageBucket: "studio-2543548682-44d30.firebasestorage.app",
+  messagingSenderId: "359400501421",
+  appId: "1:359400501421:web:b238772ba5225e622c1624"
 };
