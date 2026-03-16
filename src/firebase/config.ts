c433a3f4272dff3162807e8c76315e0fe722dc83
Rowell Indino (r.indino) <rowell.indino@neu.edu.ph>
2026-03-16 21:03:34 +0000

    ## Error Type Console FirebaseError
    ## Error Message
    Firebase: Error (a

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyB-DUMMY-KEY-FOR-STUDIO-UI",
+  apiKey: "AIzaSyC1-DUMMY-KEY-REPLACED-FOR-STUDIO",
   authDomain: "neulib-connect.firebaseapp.com",
   projectId: "neulib-connect",
   storageBucket: "neulib-connect.firebasestorage.app",
-  messagingSenderId: "123456789012",
-  appId: "1:123456789012:web:abcdef123456"
+  messagingSenderId: "782194301234",
+  appId: "1:782194301234:web:9c2b4a1f0d3e5867"
 };
